ac69d78e80c6eee85b09716bd6afec4a5be51396
Demian82 <donghyeon859@gmail.com>
2023-01-15 17:23:00 +0900

    15/Jan/2023 1722

---
+++ ProjectChap2/Project10/Chpa2_10.cpp
@@ -0,0 +1,11 @@
+/*텍스트 "I hate C++ and hate programming!"을 포함하는 텍스트 파일을 생성하라.
+파일로부터 텍스트를 읽고 "hate"를 "love"로 바꿔서 각 단어를 콘솔에 출력하는 프로그램을 작성하라.
+여러분의 프로그램은 이 문제에서 주어진 예시뿐만 아니라 텍스트에 있는 어떤 줄의 "hate"에 대해서도 동작하여야 한다.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+}
